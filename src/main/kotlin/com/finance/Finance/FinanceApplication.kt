332422946ca831fc6eeb50f1beee863cd7c16b91
Darlan Guimarães <71740181+darlangui@users.noreply.github.com>
2025-03-25 21:03:31 -0300

    initial commit
    Initial commit for struct project in Spring Boot with Kotlin.

---
+++ src/main/kotlin/com/finance/Finance/FinanceApplication.kt
@@ -0,0 +1,11 @@
+package com.finance.Finance
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinanceApplication
+
+fun main(args: Array<String>) {
+	runApplication<FinanceApplication>(*args)
+}
